4d5e03476c0fb5804a165ab889c0b5422aee04a5
Jacob Blair <jacobblair16@gmail.com>
2025-06-06 15:33:24 -0500

    add dns txt records for domain validation

--- modules/DNS/main.tf
+++ modules/DNS/main.tf
@@ -30,3 +30,26 @@ resource "azurerm_dns_a_record" "webapp_root" {
    ]
 }
 
+#Domain verification txt records
+resource "azurerm_dns_txt_record" "asuid" {
+  name = "asuid"
+  resource_group_name = var.resourcegroup
+  zone_name = azurerm_dns_zone.jacobsazlabdns.name
+  ttl = 3600
+
+  record {
+    value = "ba2537f4b7e5a24dbc5d16eab3b7178f2210ee967bc110614a3daf46c3e72844"
+  }
+}
+
+#domain verification txt record#2
+resource "azurerm_dns_txt_record" "asuid2" {
+  name = "asuid.www"
+  resource_group_name = var.resourcegroup
+  zone_name = azurerm_dns_zone.jacobsazlabdns.name
+  ttl = 3600
+
+  record {
+    value = "ba2537f4b7e5a24dbc5d16eab3b7178f2210ee967bc110614a3daf46c3e72844"
+  }
+}
